e025c00cc22d013602439f431a3d3ff60c564a9f
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-07 16:54:50 +0100

    creado el atributo string cadenaAMostrar en la clase enum direccion

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Direccion.java
@@ -1,6 +1,16 @@
 package org.iesalandalus.programacion.damas.modelo;
 
 public enum Direccion {
-    NORESTE, SURESTE, SUROESTE, NOROESTE
+    NORESTE ("Noreste"), SURESTE("Sureste"), SUROESTE("Suroeste"), NOROESTE("Noroeste");
 
+    private String cadenaAMostrar;
+    private Direccion(String cadenaAMostrar)
+    {
+        this.cadenaAMostrar= this.cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return ordinal()+".-"+cadenaAMostrar;
+    }
 }
