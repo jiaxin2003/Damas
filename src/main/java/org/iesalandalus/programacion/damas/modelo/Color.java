633ae01bddc64a16eeb73cc0069eb9f553301344
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-12 18:24:45 +0100

    creado el enumerado color que contiene negro y blanco

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Color.java
@@ -1,4 +1,16 @@
 package org.iesalandalus.programacion.damas.modelo;
 
-public class Color {
+public enum Color {
+    BLANCO("Blanco"), NEGRO("Negro");
+
+    private String cadenaAMostrar;
+    private Color(String cadenaAMostrar)
+    {
+        this.cadenaAMostrar=cadenaAMostrar;
+    }
+
+    @Override
+    public String toString() {
+        return ordinal()+".-"+cadenaAMostrar;
+    }
 }
