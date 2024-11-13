49b3d4a983b2c920c986b4f8153a51852caa1f21
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-13 23:10:13 +0100

    creado el constructor en damas para que te ponga la dama de color blanco en posiciones aleatorias que esten en las filas 1 a la 3 y en casillas negras

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -4,4 +4,61 @@ public class Dama {
     private Color colorDama;
     private Posicion posicionDama;
     private boolean esDamaEspecial;
+
+
+
+    public Dama(){
+        int fila= (int) (Math.random() * 3) + 1;
+        int columna;
+        if(fila%2==0){
+            do {
+                columna= (int) (Math.random() * 8) + 1;
+            }while (columna%2==0);
+        }else {
+            do {
+                columna = (int) (Math.random() * 8) + 1;
+            } while (columna%2 != 0);
+        }
+        char letra = switch (columna) {
+            case 1 -> 'A';
+            case 2 -> 'B';
+            case 3 -> 'C';
+            case 4 -> 'D';
+            case 5 -> 'E';
+            case 6 -> 'F';
+            case 7 -> 'G';
+            case 8 -> 'H';
+            default -> ' ';
+        };
+
+        setColorDama(Color.BLANCO);
+        setPosicionDama(new Posicion(fila,letra));
+    }
+
+
+
+
+    public Color getColorDama() {
+        return colorDama;
+    }
+
+    public void setColorDama(Color colorDama) {
+        this.colorDama = colorDama;
+    }
+
+    public Posicion getPosicionDama() {
+        return posicionDama;
+    }
+
+    public void setPosicionDama(Posicion posicionDama) {
+        this.posicionDama = posicionDama;
+    }
+
+    public boolean isEsDamaEspecial() {
+        return esDamaEspecial;
+    }
+
+    public void setEsDamaEspecial(boolean esDamaEspecial) {
+        this.esDamaEspecial = esDamaEspecial;
+    }
 }
