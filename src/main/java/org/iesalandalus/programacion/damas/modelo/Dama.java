8ee19a7d87011d05f0977f19429b0193e397957c
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-13 23:42:42 +0100

    creado metodo posicioninicial comprobando si es negro o blanco y dandoles posiciones aleatorias en las filas 1 2 y 3 si es blanca y si es negra en las filas 6 7 y 8

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -1,38 +1,51 @@
 package org.iesalandalus.programacion.damas.modelo;
 
+import java.util.EventListener;
+
 public class Dama {
     private Color colorDama;
     private Posicion posicionDama;
     private boolean esDamaEspecial;
 
 
+    private Posicion crearPosicionInicial(){
+        int fila;
+        if (colorDama == Color.BLANCO) {
+            fila = (int) (Math.random() * 3) + 1;
+        } else  {
+            fila = (int) (Math.random() * 3) + 6;
+        }
+
+
+            int columna;
+            if (fila % 2 == 0) {
+                do {
+                    columna = (int) (Math.random() * 8) + 1;
+                } while (columna % 2 == 0);
+            } else {
+                do {
+                    columna = (int) (Math.random() * 8) + 1;
+                } while (columna % 2 != 0);
+            }
+            char letra = switch (columna) {
+                case 1 -> 'A';
+                case 2 -> 'B';
+                case 3 -> 'C';
+                case 4 -> 'D';
+                case 5 -> 'E';
+                case 6 -> 'F';
+                case 7 -> 'G';
+                case 8 -> 'H';
+                default -> ' ';
+            };
+        return new Posicion(fila,letra);
+    }
+
 
     public Dama(){
-        int fila= (int) (Math.random() * 3) + 1;
-        int columna;
-        if(fila%2==0){
-            do {
-                columna= (int) (Math.random() * 8) + 1;
-            }while (columna%2==0);
-        }else {
-            do {
-                columna = (int) (Math.random() * 8) + 1;
-            } while (columna%2 != 0);
-        }
-        char letra = switch (columna) {
-            case 1 -> 'A';
-            case 2 -> 'B';
-            case 3 -> 'C';
-            case 4 -> 'D';
-            case 5 -> 'E';
-            case 6 -> 'F';
-            case 7 -> 'G';
-            case 8 -> 'H';
-            default -> ' ';
-        };
-
+        Posicion posicion=crearPosicionInicial();
         setColorDama(Color.BLANCO);
-        setPosicionDama(new Posicion(fila,letra));
+        setPosicionDama(posicion);
     }
 
 
