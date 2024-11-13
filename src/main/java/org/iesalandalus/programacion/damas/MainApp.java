101f476e7e50f62095570c6b00ec83a1258f23d7
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-13 22:13:48 +0100

    creado el contructor en la clase posicion con sus excepciones

--- src/main/java/org/iesalandalus/programacion/damas/MainApp.java
+++ src/main/java/org/iesalandalus/programacion/damas/MainApp.java
@@ -1,6 +1,9 @@
 package org.iesalandalus.programacion.damas;
 
 public class MainApp {
+    public void Main(String [] args){
+        
+    }
 	
 	
 }
---
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Posicion.java
@@ -0,0 +1,41 @@
+package org.iesalandalus.programacion.damas.modelo;
+
+public class Posicion {
+
+    private int fila;
+    private char columna;
+
+
+
+
+    public Posicion(int fila, char columna){
+        setFila(fila);
+        setColumna(columna);
+    }
+
+
+
+
+
+    public int getFila() {
+        return fila;
+    }
+
+    public void setFila(int fila) {
+        if(fila<1 | fila>8) {
+            throw new IllegalArgumentException("ERROR: introduce un numero de fila valida de entre 1 y 8");
+        }
+        this.fila=fila;
+    }
+
+    public char getColumna() {
+        return columna;
+    }
+
+    public void setColumna(char columna){
+        if(columna<'a' | columna>'h') {
+            throw new IllegalArgumentException("ERROR: introduce una letra de columna valida de fila valida de entre 1 y 8");
+        }
+        this.columna = columna;
+    }
+}
