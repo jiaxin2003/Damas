d1ae6bab736ec44c27e6cfb0c85d5d0da40e2fee
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-17 11:58:16 +0100

    he creado el metodo donde compruebo si es dama especial o no si no lo es compruebo si la dama es negra o blanca y en ellas combruebo que el movimiento sea uno nada mas y que sea simpre hacia delante y si lo es mover la dama a la direccion que el usuario quiere y si es dama especial moverlo hacia delante o hacia atras

--- src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
+++ src/main/java/org/iesalandalus/programacion/damas/modelo/Dama.java
@@ -1,16 +1,18 @@
 package org.iesalandalus.programacion.damas.modelo;
 
-import java.util.EventListener;
+import javax.naming.OperationNotSupportedException;
+import java.beans.beancontext.BeanContextServicesListener;
+import java.io.ObjectStreamException;
 
 public class Dama {
-    private Color colorDama;
-    private Posicion posicionDama;
+    private Color color;
+    private Posicion posicion;
     private boolean esDamaEspecial;
 
 
     private Posicion crearPosicionInicial(){
         int fila;
-        if (colorDama == Color.BLANCO) {
+        if (color.equals(Color.BLANCO)) {
             fila = (int) (Math.random() * 3) + 1;
         } else  {
             fila = (int) (Math.random() * 3) + 6;
@@ -21,11 +23,11 @@ public class Dama {
             if (fila % 2 == 0) {
                 do {
                     columna = (int) (Math.random() * 8) + 1;
-                } while (columna % 2 == 0);
+                } while (columna % 2 != 0);
             } else {
                 do {
                     columna = (int) (Math.random() * 8) + 1;
-                } while (columna % 2 != 0);
+                } while (columna % 2 == 0);
             }
             char letra = switch (columna) {
                 case 1 -> 'A';
@@ -41,30 +43,108 @@ public class Dama {
         return new Posicion(fila,letra);
     }
 
+    public void mover(Direccion direccion, int pasos) throws OperationNotSupportedException {
+        if (direccion==null)
+            throw new NullPointerException("ERROR: la direccion no puede estar vacia ");
+        if (pasos <1){
+            throw new OperationNotSupportedException("ERROR: el numero introducido es negativo tienes que introducir un numero positivo mayor que uno");
+        }
+        if (!esDamaEspecial){
+            if (color == Color.BLANCO) {
+                if (direccion == Direccion.SURESTE || direccion == Direccion.SUROESTE) {
+                    throw new OperationNotSupportedException("El movimiento que haces no esta permitido las damas solo pueden ir hacia delante y en diagonal");
+                } else if (pasos > 1) {
+                    throw new OperationNotSupportedException("ERROR: la dama no es dama especial lo cual ahce que no pueda moverse mas de una casilla");
+                }
+
+                int nuevaFila = posicion.getFila();
+                char nuevaColumna = posicion.getColumna();
+
+                switch (direccion) {
+                    case NORESTE -> {
+                        nuevaFila++;
+                        nuevaColumna++;
+                    }
+                    case NOROESTE -> {
+                        nuevaFila++;
+                        nuevaColumna--;
+                    }
+                }
+                }else if (color ==Color.NEGRO){
+                if (direccion== Direccion.NORESTE || direccion == Direccion.NOROESTE) {
+                    throw new OperationNotSupportedException("ERROR: El movimiento que haces no esta permitido las damas solo pueden ir hacia delante y diogonal");
+                }
+            } else if (pasos>1) {
+                throw new OperationNotSupportedException("ERROR: la Dama no es Dama especial lo cuál hace que no pueda moverse mas de una casilla");
+            } int nuevaFila = posicion.getFila();
+            char nuevaColumna = posicion.getColumna();
+            switch (direccion) {
+                case SURESTE -> {
+                    nuevaFila--;
+                    nuevaColumna++;
+                }
+                case SUROESTE -> {
+                    nuevaFila--;
+                    nuevaColumna--;
+                }
+            }
+        }else {
+            int nuevaFila = posicion.getFila();
+            char nuevaColumna = posicion.getColumna();
+            switch (direccion) {
+                case NORESTE -> {
+                    nuevaFila++;
+                    nuevaColumna++;
+                }
+                case NOROESTE -> {
+                    nuevaFila++;
+                    nuevaColumna--;
+                }
+                case SURESTE -> {
+                    nuevaFila--;
+                    nuevaColumna++;
+                }
+                case SUROESTE -> {
+                    nuevaFila--;
+                    nuevaColumna--;
+                }
+            }
+        }
+    }
+
 
     public Dama(){
-        Posicion posicion=crearPosicionInicial();
-        setColorDama(Color.BLANCO);
-        setPosicionDama(posicion);
+        Posicion posicionInicial=crearPosicionInicial();
+        setColor(Color.BLANCO);
+        setPosicion(posicionInicial);
+        this.esDamaEspecial=false;
+    }
+    public Dama(Color color){
+        Posicion posicionInicial=crearPosicionInicial();
+        setColor(color);
+        setPosicion(posicionInicial);
+        this.esDamaEspecial=false;
     }
 
 
 
 
-    public Color getColorDama() {
-        return colorDama;
+    public Color getColor() {
+        return color;
     }
 
-    public void setColorDama(Color colorDama) {
-        this.colorDama = colorDama;
+    public void setColor(Color color) {
+        if (color ==null)
+            throw new NullPointerException();
+        this.color = color;
     }
 
-    public Posicion getPosicionDama() {
-        return posicionDama;
+    public Posicion getPosicion() {
+        return posicion;
     }
 
-    public void setPosicionDama(Posicion posicionDama) {
-        this.posicionDama = posicionDama;
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
     }
 
     public boolean isEsDamaEspecial() {
@@ -73,5 +153,14 @@ public class Dama {
 
     public void setEsDamaEspecial(boolean esDamaEspecial) {
         this.esDamaEspecial = esDamaEspecial;
+    }
+
+    @Override
+    public String toString() {
+        return "Dama{" +
+                "color=" + color +
+                ", posicion=" + posicion +
+                ", esDamaEspecial=" + esDamaEspecial +
+                '}';
     }
 }
