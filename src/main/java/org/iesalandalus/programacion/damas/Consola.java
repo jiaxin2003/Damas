beaf1d1382f411e2992fe3e9c2495c814ed8edab
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-17 12:14:53 +0100

    creado el metodo para elegir el color de la Dama entre blanco y negro

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.damas;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
     private Consola() {
 
@@ -9,5 +11,23 @@ public class Consola {
         System.out.println("2. Crear Dama Eligiendo color");
         System.out.println("3. Movimiento");
         System.out.println("4. Salir del Programa");
+    }
+    public static int elegirOpcionMenu(){
+        int opcion;
+            do {
+                System.out.println("Elige la opcion que quieres del 1 al 4");
+                opcion= Entrada.entero();
+            }while (opcion<1 || opcion>4);
+            return opcion;
+    }
+    public static int elegirOpcion(){
+        int colorOpcion;
+        do {
+            System.out.println("Elige un color entre Negro y Blanco");
+            System.out.println("1. Blanco");
+            System.out.println("2. negro");
+            colorOpcion=Entrada.entero();
+        }while (colorOpcion<1||colorOpcion>2);
+        return colorOpcion;
     }
 }
