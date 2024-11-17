8db3659c1abe0e8c42ed14c72242a02a1effd64b
JIAXIN <jiaxinzhang.2003@gmail.com>
2024-11-17 12:06:11 +0100

    creadoe el metodo mostrarMenu pidiendo al ususario que eliga 4 opciones

--- src/main/java/org/iesalandalus/programacion/damas/Consola.java
+++ src/main/java/org/iesalandalus/programacion/damas/Consola.java
@@ -1,4 +1,13 @@
 package org.iesalandalus.programacion.damas;
 
 public class Consola {
+    private Consola() {
+
+    }
+    public static void mostrarMenu(){
+        System.out.println("1. Crear Dama por Defecto");
+        System.out.println("2. Crear Dama Eligiendo color");
+        System.out.println("3. Movimiento");
+        System.out.println("4. Salir del Programa");
+    }
 }
